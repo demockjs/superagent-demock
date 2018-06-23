61bf4393973b664b69ead8c68fef8b4daf1ba724
Ates Goral <ates@magnetiq.com>
2018-06-23 00:27:59 -0400

    Stashing pending changes

--- superagent-demock.js
+++ superagent-demock.js
@@ -1,11 +1,38 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define([ 'superagent', 'demock' ], factory);
+        define([ 'demock' ], factory);
     } else if (typeof exports === 'object') {
-        factory(require('superagent'), require('demock'));
+        module.exports = factory(require('demock'));
     } else {
-        factory(root.superagent, root.demock);
+        root.superagentDemock = factory(root.demock);
     }
-}(this, function (superagent, Demock) {
+}(this, function (Demock) {
+	return function (request) {
+		var proto = request.Request.prototype;
 
+		var originalEnd = proto.end;
+
+		proto.end = function () {
+			console.log('end', this);
+
+			// this._header + this.header
+			// this._query
+			// this._formData + this._data
+			var request = {
+				method: this.method,
+				url: this.url, // @todo split out query params
+				headers: this.header
+			};
+
+			// to do run request through demock
+
+			var modified = Object.create(this, {
+
+			});
+
+			var modified = new Request()
+
+			return originalEnd.apply(modified, arguments);
+		};
+	};
 }));
